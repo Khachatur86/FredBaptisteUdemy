94bd9c969859d85bbf38d4272e6d5b1910538ff1
Хачатур <sarkhv@gmail.com>
2018-12-08 16:12:46 +0300

    globals() is dictionary with atributes and functions

--- modules/module_describing.py
+++ modules/module_describing.py
@@ -1,1 +1,13 @@
 # Начало новой темы модули
+def func():
+    a = 10
+    return a
+
+
+print(func)
+print(id(func))
+print(globals())
+print(type(globals()))  # тип объекта словарь
+f = globals()["func"]  # это наша функция определенная выше
+print(f is func) # True
+
