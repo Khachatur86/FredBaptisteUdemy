c2c7039f1ec2de2b7c2f28bf826c32a3ef62516f
Хачатур <sarkhv@gmail.com>
2018-12-08 16:18:12 +0300

    What is locals()?

--- modules/module_describing.py
+++ modules/module_describing.py
@@ -9,5 +9,15 @@ print(id(func))
 print(globals())
 print(type(globals()))  # тип объекта словарь
 f = globals()["func"]  # это наша функция определенная выше
-print(f is func) # True
+print(f is func)  # True
 
+print(locals())
+
+
+def func():
+    a = 10
+    b = 10
+    print(locals())
+
+
+func()  # locals() - словарь с переменными функции func
