396d42127fa883bfea7ffd424e540f81584b7490
Хачатур <sarkhv@gmail.com>
2018-12-23 19:15:16 +0300

    Tic_tac_toe

--- checkio/tic_tac_toe.py
+++ checkio/tic_tac_toe.py
@@ -1,7 +1,20 @@
-from typing import List
+def checkio(game_result):
 
-def checkio(game_result: List[str]) -> str:
-    return "D" or "X" or "O"
+    O = 'OOO'
+    X = 'XXX'
+
+    game_result.append(game_result[0][0] + game_result[1][1] + game_result[2][2])  # m_diag
+    game_result.append(game_result[0][2] + game_result[1][1] + game_result[2][0])  # s_diag
+    game_result.append(game_result[0][0] + game_result[1][0] + game_result[2][0])  # c0
+    game_result.append(game_result[0][1] + game_result[1][1] + game_result[2][1])  # c1
+    game_result.append(game_result[0][2] + game_result[1][2] + game_result[2][2])  # c2
+
+    for row in game_result:
+        if row == X:
+            return 'X'
+        if row == O:
+            return 'O'
+    return "D"
 
 if __name__ == '__main__':
     print("Example:")
