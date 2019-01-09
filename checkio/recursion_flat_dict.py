e6332a968bc7ee63b362f3126933f6ad1909f5ff
Хачатур <sarkhv@gmail.com>
2019-01-09 21:58:19 +0300

    Update

--- checkio/recursion_flat_dict.py
+++ checkio/recursion_flat_dict.py
@@ -1,1 +1,26 @@
-import requests
+def flatten(dictionary):
+    stack = [((), dictionary)]
+    result = {}
+    while stack:
+        path, current = stack.pop()
+        print("path - ", path)
+        print("current - ",current)
+        for k, v in current.items():
+            if isinstance(v, dict) and v:
+                stack.append((path + (k,), v))
+                print("Stack3 - ",stack)
+            else:
+                result["/".join((path + (k,)))] = v or ""
+                print("result - ", result)
+    return result
+
+test1 = {"key": {"deeper": {"more": {"enough": "value"}},
+                 "deeper2": 2,
+                  "deeper3": {"deeper4": 1}}}
+
+flatten(test1)
+# stack = [((), dictionary)]
+# print(stack)
+# print(stack.pop())
+# print(stack)
+
