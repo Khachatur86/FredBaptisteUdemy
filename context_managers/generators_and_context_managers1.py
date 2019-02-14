abef980496ff61809cf4bc29afbcb2fa65254e31
Хачатур <sarkhv@gmail.com>
2019-02-14 22:22:41 +0300

    generators_and_context_managers1

---
+++ context_managers/generators_and_context_managers1.py
@@ -0,0 +1,37 @@
+def my_gen():
+  try:
+    print('creating context and yielding object')
+    yield [1, 2, 3, 4]
+  finally:
+    print('exiting context and cleaning up')
+
+# gen = my_gen()
+# lst = next(gen)
+# print(lst)
+# # next(gen) # StopIteration exception
+# gen = my_gen()
+# lst = next(gen)
+# print(lst)
+# try:
+#   next(gen)
+# except StopIteration:
+#   pass
+
+class GenCtxManager:
+  def __init__(self, gen_func):
+    self._gen = gen_func()
+
+  def __enter__(self):
+    return next(self._gen)
+
+  def __exit__(self, exc_type, exc_value, exc_tb):
+    try:
+      next(self._gen)
+    except StopIteration:
+      pass
+    return False
+
+with GenCtxManager(my_gen) as obj:
+  print(obj)
+
+
