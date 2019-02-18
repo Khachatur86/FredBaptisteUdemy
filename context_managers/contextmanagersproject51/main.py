85d777ba30a7bc8ef48c44f3b7315f298ed539bf
Хачатур <sarkhv@gmail.com>
2019-02-18 23:01:55 +0300

    project 5_1

---
+++ context_managers/contextmanagersproject51/main.py
@@ -0,0 +1,54 @@
+f_names = 'cars.csv', 'personal_info.csv'
+
+for f_name in f_names:
+  with open(f_name) as f:
+    print(next(f), end='')
+    print(next(f), end='')
+  print('\n--------------------')
+
+import csv
+from itertools import islice
+
+with open(f_names[0]) as f:
+  dialect = csv.Sniffer().sniff(f.read(1000))
+
+
+def get_dialect(f_name):
+  with open(f_name) as f:
+    return csv.Sniffer().sniff(f.read(1000))
+
+from collections import namedtuple
+
+class FileParser:
+  def __init__(self, f_name):
+    self.f_name = f_name
+  
+  def __enter__(self):
+    self._f = open(self.f_name, 'r')
+    self._reader = csv.reader(self._f, get_dialect(self.f_name))
+    headers = map(lambda s: s.lower(),next(self._reader))
+    self._nt = namedtuple('Data', headers)
+    return self
+
+  def __exit__(self, exc_type, exc_value, exc_tb):
+    self._f.close()
+    return False
+
+  def __iter__(self):
+    return self
+  
+  def __next__(self):
+    if self._f.closed:
+      raise StopIteration
+    else:
+      return self._nt(*next(self._reader))
+
+
+with FileParser('cars.csv') as data:
+  # for row in islice(data, 10):
+    print(list(data))
+
+
+# with FileParser('personal_info.csv') as data:
+#   for row in islice(data, 10):
+#     print(row)
